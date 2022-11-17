54fe9d5f35723b90d8a9780486056e01a6bb3479
Rajeshwar Verma <rajeshwar2.nirmal.labs@gmail.com>
2022-11-17 16:46:44 +0530

    update

--- addstring.js
+++ addstring.js
@@ -1,14 +1,10 @@
 // Question 1:
 var str1 = 'Today is';
-var str2 = '      a beautiful day     '
-var str3 = ' In Hawaii.     '
-// Result = 'Today is a beautiful day In Hawaii.'
-// Use the str1, str2, str3 variables to create the Result string. Keep the extra spaces, lowercase and uppercase letters in mind.
-console.log(str1);
-console.log(str2);
-console.log(str3);
+var str2 = '      a beautiful day     ';
+var str3 = ' In Hawaii.     ';
+var Result;
 str2=str2.trim();
 str3=str3.trim();
-// console.log(st);
+Result=(str1+str2+str3);
 console.log(str1+str2+str3);
-document.write(str1+str2+str3);
+document.write(Result);
--- triangletypecheck.js
+++ triangletypecheck.js
@@ -1,12 +1,4 @@
-// Question 4:
-// Write a JavaScript program to input sides of a triangle and check whether a triangle is equilateral, scalene or isosceles triangle using if else.
-// Properties of triangle:
-// A triangle is said Equilateral Triangle, if all its sides are equal. If a, b, c are three sides of triangle. Then, the triangle is equilateral only if a == b == c.
-// A triangle is said Isosceles Triangle, if its two sides are equal. If a, b, c are three sides of triangle. Then, the triangle is isosceles if either a == b or a == c or b == c.
-// A triangle is said Scalene Triangle, if none of its sides are equal.
-// let a = 10;
-// let b = 20;
-// let c = 30;
+
 function checkTriangle() {
     
     const a = document.getElementById("num1").value;
